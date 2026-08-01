videolan/vlc

dvdread: describe tracks from code_extension

modules/access/dvdread.c
@@ -48,6 +48,7 @@
 #include <vlc_input.h>
 #include <vlc_charset.h>
 #include "disc_helper.h"
+#include "dvd_description.h"
 
 
 /*****************************************************************************
@@ -860,7 +861,7 @@ static int DemuxBlock( demux_t *p_demux, const uint8_t *p, int len )
 
                 if( !tk->b_configured )
                 {
-                    DvdReadESNew( p_demux, i_id, 0 );
+                    DvdReadESNew( p_demux, i_id, 0, 0 );
                 }
                 if( tk->es &&
                     !ps_pkt_parse_pes( VLC_OBJECT(p_demux), p_pkt, tk->i_skip ) )
@@ -914,7 +915,7 @@ static int DemuxBlock( demux_t *p_demux, const uint8_t *p, int len )
 /*****************************************************************************
  * ESNew: register a new elementary stream
  *****************************************************************************/
-void DvdReadESNew( demux_t *p_demux, int i_id, int i_lang )
+void DvdReadESNew( demux_t *p_demux, int i_id, int i_lang, int i_code_ext )
 {
     demux_sys_t *p_sys = p_demux->p_sys;
     ps_track_t  *tk = &p_sys->tk[ps_id_to_tk(i_id)];
@@ -966,6 +967,11 @@ void DvdReadESNew( demux_t *p_demux, int i_id, int i_lang )
 #endif
 
         if( psz_language[0] ) tk->fmt.psz_language = strdup( psz_language );
+
+        if( (size_t) i_code_ext < ARRAY_SIZE(dvd_audio_code_ext)
+            && dvd_audio_code_ext[i_code_ext] )
+            tk->fmt.psz_description =
+                strdup( vlc_gettext( dvd_audio_code_ext[i_code_ext] ) );
     }
     else if( tk->fmt.i_cat == SPU_ES )
     {
@@ -976,6 +982,11 @@ void DvdReadESNew( demux_t *p_demux, int i_id, int i_lang )
         memcpy( tk->fmt.subs.spu.palette, p_sys->clut, sizeof( p_sys->clut ) );
 
         if( psz_language[0] ) tk->fmt.psz_language = strdup( psz_language );
+
+        if( (size_t) i_code_ext < ARRAY_SIZE(dvd_spu_code_ext)
+            && dvd_spu_code_ext[i_code_ext] )
+            tk->fmt.psz_description =
+                strdup( vlc_gettext( dvd_spu_code_ext[i_code_ext] ) );
     }
 
     tk->es = es_out_Add( p_demux->out, &tk->fmt );

Olsro/powervlc

(cherry picked from commit 7e242296b4d6d483cd4054ce7ab6d5f6c6020c81)

modules/access/dvdread.c
@@ -45,6 +45,7 @@
 #include <vlc_charset.h>
 #include <vlc_interface.h>
 #include <vlc_dialog.h>
+#include "dvd_description.h"
 
 #include <vlc_iso_lang.h>
 
@@ -157,7 +158,7 @@ static int Demux     ( demux_t * );
 static int DemuxBlock( demux_t *, const uint8_t *, int );
 
 static void DemuxTitles( demux_t *, int * );
-static void ESNew( demux_t *, int, int );
+static void ESNew( demux_t *, int, int, int );
 
 static int  DvdReadSetArea  ( demux_t *, int, int, int );
 static int  DvdReadSeek     ( demux_t *, int );
@@ -658,7 +659,7 @@ static int DemuxBlock( demux_t *p_demux, const uint8_t *p, int len )
 
                 if( !tk->b_configured )
                 {
-                    ESNew( p_demux, i_id, 0 );
+                    ESNew( p_demux, i_id, 0, 0 );
                 }
                 if( tk->es &&
                     !ps_pkt_parse_pes( VLC_OBJECT(p_demux), p_pkt, tk->i_skip ) )
@@ -688,7 +689,7 @@ static int DemuxBlock( demux_t *p_demux, const uint8_t *p, int len )
 /*****************************************************************************
  * ESNew: register a new elementary stream
  *****************************************************************************/
-static void ESNew( demux_t *p_demux, int i_id, int i_lang )
+static void ESNew( demux_t *p_demux, int i_id, int i_lang, int i_code_ext )
 {
     demux_sys_t *p_sys = p_demux->p_sys;
     ps_track_t  *tk = &p_sys->tk[ps_id_to_tk(i_id)];
@@ -739,6 +740,11 @@ static void ESNew( demux_t *p_demux, int i_id, int i_lang )
 #endif
 
         if( psz_language[0] ) tk->fmt.psz_language = strdup( psz_language );
+
+        if( (size_t) i_code_ext < ARRAY_SIZE(dvd_audio_code_ext)
+            && dvd_audio_code_ext[i_code_ext] )
+            tk->fmt.psz_description =
+                strdup( vlc_gettext( dvd_audio_code_ext[i_code_ext] ) );
     }
     else if( tk->fmt.i_cat == SPU_ES )
     {
@@ -748,6 +754,11 @@ static void ESNew( demux_t *p_demux, int i_id, int i_lang )
                 16 * sizeof( uint32_t ) );
 
         if( psz_language[0] ) tk->fmt.psz_language = strdup( psz_language );
+
+        if( (size_t) i_code_ext < ARRAY_SIZE(dvd_spu_code_ext)
+            && dvd_spu_code_ext[i_code_ext] )
+            tk->fmt.psz_description =
+                strdup( vlc_gettext( dvd_spu_code_ext[i_code_ext] ) );
     }
 
     tk->es = es_out_Add( p_demux->out, &tk->fmt );
@@ -911,7 +922,7 @@ static int DvdReadSetArea( demux_t *p_demux, int i_title, int i_chapter,
         /* TODO: re-add angles */
 
 
-        ESNew( p_demux, 0xe0, 0 ); /* Video, FIXME ? */
+        ESNew( p_demux, 0xe0, 0, 0 ); /* Video, FIXME ? */
         const video_attr_t *p_attr = &p_vts->vtsi_mat->vts_video_attr;
         int i_video_height = p_attr->video_format != 0 ? 576 : 480;
         int i_video_width;
@@ -987,7 +998,9 @@ static int DvdReadSetArea( demux_t *p_demux, int i_title, int i_chapter,
                 }
 
                 ESNew( p_demux, i_id, p_sys->p_vts_file->vtsi_mat->
-                       vts_audio_attr[i - 1].lang_code );
+                       vts_audio_attr[i - 1].lang_code,
+                       p_sys->p_vts_file->vtsi_mat->
+                       vts_audio_attr[i - 1].code_extension );
             }
         }
 #undef audio_control
@@ -1037,7 +1050,9 @@ static int DvdReadSetArea( demux_t *p_demux, int i_title, int i_chapter,
                 i_id = (0x20 + i_position) | PS_PACKET_ID_MASK_VOB;
 
                 ESNew( p_demux, i_id, p_sys->p_vts_file->vtsi_mat->
-                       vts_subp_attr[i - 1].lang_code );
+                       vts_subp_attr[i - 1].lang_code,
+                       p_sys->p_vts_file->vtsi_mat->
+                       vts_subp_attr[i - 1].code_extension );
             }
         }
 #undef spu_control